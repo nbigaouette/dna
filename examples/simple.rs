cf76f4d4bb1885a114150586af31e35797600b70
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-05-19 13:21:33 -0400

    Pass a string to lib

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,4 +3,6 @@ extern crate dna;
 fn main() {
     println!("Hello from simple.rs");
     dna::test();
+
+    dna::read_steps("simple.toml");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,8 @@ mod tests {
 pub fn test() {
     println!("Hello from lib.rs");
 }
+
+
+pub fn read_steps(filename: &str) {
+    println!("Reading file {:?}", filename);
+}
