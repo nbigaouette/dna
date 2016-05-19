03c44a96d69d8ec641c89be928c5a60b0de9a215
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-05-19 13:41:57 -0400

    New function to parse toml file, unimplemented

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,26 @@ pub fn test() {
     println!("Hello from lib.rs");
 }
 
+#[derive(Debug)]
+enum Steps {
+    Echo,
+    Run,
+    Shell,
+    SetEnv,
+}
+
+
+fn parse_toml(filename: &str) -> Vec<Steps> {
+
+    println!("Reading toml file: {}", filename);
+
+    unimplemented!();
+}
 
 pub fn execute_steps(filename: &str) {
     println!("Reading file {:?}", filename);
+
+    let steps = parse_toml(filename);
+
+    println!("Steps: {:?}", steps);
 }
